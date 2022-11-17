93ef0e0ff7b11903fba40b9897dd66064e969bf9
JeevaVelan <105722798+JeevaVelan@users.noreply.github.com>
2022-11-17 14:57:43 +0530

    Add files via upload

---
+++ app.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>app screen</title>
+</head>
+<body>
+<link rel="stylesheet" href="style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Chilanka&family=Dancing+Script:wght@700&display=swap" rel="stylesheet">
+    <nav>
+                <ul id='MenuItems'>
+                   
+                    <li> <a href="logout.php"><button>Logout</button></li>
+                    
+                </ul>
+            </nav>
+   
+    
+    <h1>Hi! Guys welcome to Seek Coding Youtube Channel<br>If You Are New to my channel, please     do Subscribe to my channel and <br> like, share video if you found the tutorial is interesting...
+    <br>Thankyou.....</h1>
+    
+</body>
+</html>
